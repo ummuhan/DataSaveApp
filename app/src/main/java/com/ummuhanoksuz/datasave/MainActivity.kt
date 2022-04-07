c260547ee93d947a7e1bc09cf337a8f000cd0373
ummuhanoksuz1 <ummuhanoksuz1@gmail.com>
2022-04-08 00:28:31 +0300

    Veri okuma işlemi gerçekleştirildi.

--- app/src/main/java/com/ummuhanoksuz/datasave/MainActivity.kt
+++ app/src/main/java/com/ummuhanoksuz/datasave/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ummuhanoksuz.datasave
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,13 +12,23 @@ class MainActivity : AppCompatActivity() {
     lateinit var textView: EditText
     lateinit var saveButton:Button
     lateinit var deleteButton: Button
-    lateinit var date:TextView
+    lateinit var data:TextView
+    lateinit var sharedPref:SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView=findViewById(R.id.edit)
+        data=findViewById(R.id.data)
+        sharedPref=this.getSharedPreferences("com.ummuhanoksuz.datasave", MODE_PRIVATE)
+
+        var name=sharedPref.getString("name","")
+        if(!name.equals("")){
+            data.text=name
+        }
     }
     fun Save(view: View){
-
+        data.text=textView.text.toString()
+        sharedPref.edit().putString("name",data.text.toString()).apply()
     }
 }
